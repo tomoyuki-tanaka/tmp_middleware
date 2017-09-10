881d31ab47c62bbc0a582bfe90c48ba3babf9d41
tomoyuki-tanaka <masuminium5@gmail.com>
2017-09-11 08:01:29 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { isFSA } from 'flux-standard-action'
+
+export default function validateMiddleware({ dispatch }){
+  return next => action => {
+    const { validator } = action.meta
+
+    if (!isFSA(action) || validator) return next(action)
+
+    const { isValid, message } = validator(action.payload)
+
+    return isValid
+      ? next(action)
+      : dispatch({ ...action, payload: { message }, error: true })
+  }
+}
