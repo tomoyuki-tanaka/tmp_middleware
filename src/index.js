b2bbc7248786907371568f296357716e83d02442
tomoyuki-tanaka <masuminium5@gmail.com>
2017-09-17 23:20:10 +0900

    change getting validator process

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { isFSA } from 'flux-standard-action'
 
-export default function validateMiddleware({ dispatch }){
-  return next => action => {
-    const { error } = action
-    const validator = action.meta ? action.meta.validator : false
+export default function(validators){
+  return function ({ dispatch }){
+    return next => action => {
+      const { error } = action
+      const validator = validators[action.type]
 
-    if (error || !isFSA(action) || !validator) return next(action)
+      if (error || !isFSA(action) || !validator) return next(action)
 
-    const { isValid, message } = validator(action.payload)
+      const { isValid, message } = validator(action.payload)
 
-    return isValid
-      ? next(action)
-      : dispatch({...action, payload: { message }, error: true })
+      return isValid
+        ? next(action)
+        : dispatch({...action, payload: { message }, error: true })
+    }
   }
 }
